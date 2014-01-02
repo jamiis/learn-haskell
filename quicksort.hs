5e4ff4aaa6c8b3b58df6734d9d9d95d0f563c318
Jamis Johnson <jamismanwaring@gmail.com>
2014-01-02 15:39:08 -0700

    use partition in quicksort instead of list comprehension

--- quicksort.hs
+++ quicksort.hs
@@ -1,6 +1,7 @@
+import Data.List
+
 quicksort :: (Ord a) => [a] -> [a]
 quicksort [] = []
 quicksort (x:xs) = 
-    let smallSort = quicksort [a | a <- xs, a <= x]
-        bigSort   = quicksort [a | a <- xs, a > x]
-    in smallSort ++ [x] ++ bigSort
+    quicksort small ++ [x] ++ quicksort big
+    where (small,big) = partition (<=x) xs
